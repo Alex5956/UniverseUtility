a706cb3f2d0a89c6bbfcba79bf7ab2719d2182f0
Alexandre Marillesse <alexandre.marillesse@gmail.com>
2021-08-19 19:44:52 +0200

    commit du 19 aout mise a jour packages

--- assets/js/uploadedvideo.js
+++ assets/js/uploadedvideo.js
@@ -6,46 +6,43 @@ const progressBarFill= document.querySelector("#progressBar > .progress-bar-fill
 const progressBarText= progressBarFill.querySelector(".progress-bar-text");
 
 uploadForm.addEventListener("submit",uploadFile);
-//
-//
-// console.log(uploadDataForm);
 function uploadFile(e) {
     let uploadDataForm = new FormData(uploadForm);
 //uploadDataForm.append('file' ,'fichier');
     for(var value of uploadDataForm.values()) {
         console.log(value);
     }
-    $.ajax({
-        type: "POST",
-        url: '/' ,
-        data: {
-             uploadDataForm
-        },
-        enctype: 'multipart/form-data',
-        processData: false,
-        contentType: false,
-        error: function (error){
-            console.error(error);
-        },
-        success: function(response) {
-
-        }
-    });
+    // $.ajax({
+    //     type: "POST",
+    //     url: '/' ,
+    //     data: {
+    //          uploadDataForm
+    //     },
+    //     enctype: 'multipart/form-data',
+    //     processData: false,
+    //     contentType: false,
+    //     error: function (error){
+    //         console.error(error);
+    //     },
+    //     success: function(response) {
+    //
+    //     }
+    // });
     e.preventDefault();
-    // try {
-    //     const xhr = new XMLHttpRequest();
-    //     xhr.open('POST', '/');
-    //     console.log('coucou');
-    //     xhr.upload.addEventListener('progress', e => {
-    //         const percent = e.lengthComputable ? (e.loaded / e.total) * 100 : 0;
-    //         console.log('coucou2');
-    //         progressBarFill.width = percent.toFixed(2)
-    //     });
-    //     xhr.setRequestHeader("Content-Type", "multipart/form-data");
-    //     xhr.send(new FormData(uploadForm));
-    // }
-    // catch (error){
-    //     console.error(error);
-    // }
+    try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/');
+        console.log('coucou');
+        xhr.upload.addEventListener('progress', e => {
+            const percent = e.lengthComputable ? (e.loaded / e.total) * 100 : 0;
+            console.log('coucou2');
+            progressBarFill.width = percent.toFixed(2)
+        });
+        xhr.setRequestHeader("Content-Type", "multipart/form-data");
+        xhr.send(uploadDataForm);
+    }
+    catch (error){
+        console.error(error);
+    }
 
 }
